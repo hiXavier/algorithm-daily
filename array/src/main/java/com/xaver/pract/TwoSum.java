09c115a78d28e79b113e6974a7681220a47d9913
Xavier5 <244483096@qq.com>
2018-02-05 22:11:23 +0800

    tow sum

---
+++ array/src/main/java/com/xaver/pract/TwoSum.java
@@ -0,0 +1,46 @@
+package com.xaver.pract;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @authore wuyanfeng
+ * @description Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * @date 2018/2/5
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] indexArray = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > 0 && i != j; j--) {
+                if (nums[i] + nums[j] == target) {
+                    indexArray[0] = i;
+                    indexArray[1] = j;
+                    break;
+                }
+            }
+        }
+        return indexArray;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int[] indexArray = new int[2];
+        Map<Integer,Integer> indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if(indexMap.containsKey(target - nums[i])){
+                if (nums[i] + nums[j] == target) {
+                    indexArray[0] = i;
+                    indexArray[1] = j;
+                    break;
+                }
+            }
+        }
+        return indexArray;
+    }
+}
